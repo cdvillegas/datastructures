3102489c51fd94e5dbeb47d4b585dd1e827fd277
cdvillegas <cdvillegas11@gmail.com>
2019-10-23 14:04:21 -0700

    Naming changes in disjointsets tests

--- test/test_disjoinsets.py
+++ test/test_disjoinsets.py
@@ -9,10 +9,10 @@ class TestDisjointSets(unittest.TestCase):
 	def test_build(self):
 		for case_type in ['one set', 'two sets']:
 			cases = testutil.parse_cases('disjointsets', case_type)
-			solutions = testutil.parse_solutions(case_type, 'disjointsets', case_type)
+			solutions = testutil.parse_solutions('build', 'disjointsets', case_type)
 			solutions = [[set(solution_set) for solution_set in solution] for solution in solutions ]
 			for case, solution in zip(cases, solutions):
-				self.assertEqual(case.sets(), solution, testutil.error_str(case, solution, 'disjointsets', case_type))
+				self.assertEqual(case.sets(), solution, testutil.error_str(case, solution, 'disjointsets', 'build'))
 
 if __name__ == '__main__':
 	unittest.main()
