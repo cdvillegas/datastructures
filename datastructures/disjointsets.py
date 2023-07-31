ca3431a20b556500af1870b20a566a8b7283295e
cdvillegas <cdvillegas11@gmail.com>
2023-07-31 15:02:50 -0700

    Simplified disjoint sets class

--- datastructures/disjointsets.py
+++ datastructures/disjointsets.py
@@ -1,42 +1,35 @@
+from collections import defaultdict
+
 class DisjointSets:
-	def __init__(self, val, pairs=None):
-		if not pairs:
-			pairs = []
-		self.nodes = self.build(pairs, val)
+	"""
+	This data structure keeps track of a set of elements partitioned
+	into several disjoin subsets. Two sets are disjoint when they 
+	have no elements in common.
 
-	def __str__(self):
-		return ''.join([str(list(unique_set)) + ' ' for unique_set in self.sets()])
+	This data structure supports two key methods:
 
-	def sets(self):
-		sets = dict()
-		for child in self.nodes.keys():
-			children = set()
-			while child != self.nodes[child]:
-				children.add(child)
-				child = self.nodes[child]
-			sets[child] = sets.get(child, set()).union(children)
-		roots = self.roots()
-		return [sets[root].union(set([root])) for root in roots]
+	find() : Determines which subset an element belongs to by 
+				returning its parent node
 
+	union() : Merges two different subsets into a single subset
+	"""
 
-	def build(self, pairs, val):
-		roots = dict()
-		if isinstance(val, str):
-			for item in val:
-				roots[item] = item
-		elif isinstance(val, int):
-			for item in range(val):
-				roots[item] = item
+	def __init__(self, pairs=[]):
+		self.parents = defaultdict(lambda: -1)
+		self.add(pairs)
 
-		for root, child in pairs:
-			child = roots.setdefault(child, child)
-			while child != roots[child]:
-				child = roots[child]
-			roots[child] = root
-		return roots
+	def add(self, pairs):
+		for a, b in pairs:
+			self.union(a, b)
 
-	def roots(self):
-		return [key for key, val in self.nodes.items() if key == val]
+	def find(self, node):
+		if x != self.parents[node]: 
+			self.parents[node] = find(self.parents[node])
+
+		return self.parents[node]
+
+	def union(self, x, y):
+		self.parents[find(y)] = find(x)
 
 
 
