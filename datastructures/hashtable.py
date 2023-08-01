d165d42e5aaa5cdf3d100fb2a76d0f78519ecbbd
cdvillegas <cdvillegas11@gmail.com>
2023-07-31 17:00:05 -0700

    Simplified HashTable

--- datastructures/hashtable.py
+++ datastructures/hashtable.py
@@ -1,79 +1,42 @@
 class HashTable:
+	"""
+	A HashTable is a data structure that provides a mapping 
+	between keys and values using a hashing function. It 
+	allows efficient retrieval, insertion, and deletion of 
+	elements. The keys are hashed into indices of an array, 
+	and values are stored at those indices. In case of hash 
+	collisions, chaining is used to store multiple key-value 
+	pairs at the same index.
+	"""
 	def __init__(self, size):
-		self.table = [[] for _ in range(size)]
-		self.size = size
-
-
-	def __str__(self):
-		s = ''
-		for bucket in self.table:
-			for key, val in bucket:
-				s += '(' + str(key) + ' => ' + str(val) + ')'
-		return s
-
+	    self.table = [[] for _ in range(size)]
+	    self.size = size
 
 	def put(self, key, val):
-		''' Put key value pair (key, val)
-		Args:
-			key: Hashable search key
-
-			val: Value to put into the table
-
-		Returns:
-			None
-		'''
-
-		index = hash(key) % self.size
-		chain = self.table[index]
-		for i in range(len(chain)):
-			if chain[i][0] == key:
-				chain[i] = (key, val)
-				return
-		self.table[index].append((key, val))
-
+	    # Compute the index using the hash function
+	    index = hash(key) % self.size
+	    chain = self.table[index]
+	    
+	    # Search the chain to see if the key already exists
+	    # If it does, overwrite it
+	    for i in range(len(chain)):
+	        if chain[i][0] == key:
+	            chain[i] = (key, val)
+	            return
+	    
+	    # If the key does not exist, append a new key-value 
+	    # tuple to the chain
+	    self.table[index].append((key, val))
 
 	def get(self, key):
-		''' Get value associated with key
-		Args:
-			key: Hashable search key
+	    # Compute the index using the hash function
+	    index = hash(key) % self.size
 
-		Returns:
-			Value if key exists in table;
-			Raises KeyError if key does
-			not exist
-		'''
-
-		index = hash(key) % self.size
-		for k, v in self.table[index]:
-			if k == key:
-				return v
-		raise KeyError
-
-
-	@staticmethod
-	def evaluate(actions):
-		''' Execute list of actions
-		Args:
-			actions: List of strings
-				representing actions, each
-				beginning with the function
-				name followed by function
-				arguments
-
-		Returns:
-			List of results of evaluating
-			each action
-		'''
-
-		table = None
-		res = []
-		for action in actions:
-			action = action.split(' ')
-			if action[0] == 'init':
-				table = HashTable(int(action[1]))
-				res.append(None)
-			elif action[0] == 'put':
-				res.append(table.put(action[1], action[2]))
-			elif action[0] == 'get':
-				res.append(table.get(action[1]))
-		return res
+	    # Search the chain for the key and return 
+	    # the corresponding value
+	    for k, v in self.table[index]:
+	        if k == key:
+	            return v
+	    
+	    # If the key does not exist, raise a KeyError
+	    raise KeyError
--- tests/test_hashtable.py
+++ tests/test_hashtable.py
@@ -1,17 +1,44 @@
-import sys
-sys.path.insert(0, '..')
-
-from util import testutil
 import unittest
+from datastructures.hashtable import HashTable
 
 class TestHashTable(unittest.TestCase):
-	def test_get(self):
-		cases = testutil.parse_cases('hashtable', 'get')
-		solutions = testutil.parse_solutions('get', 'hashtable', 'get')
 
-		for result, solution in zip(cases, solutions):
-			self.assertEqual(result, solution, testutil.error_str(result, solution, 'hashtable', 'get'))
+    def test_put_and_get(self):
+        ht = HashTable(10)
+        ht.put('a', 1)
+        self.assertEqual(ht.get('a'), 1)
+
+        ht.put('b', 2)
+        self.assertEqual(ht.get('b'), 2)
+
+    def test_collision_handling(self):
+        ht = HashTable(1)
+        ht.put('a', 1)
+        ht.put('b', 2)
+
+        self.assertEqual(ht.get('a'), 1)
+        self.assertEqual(ht.get('b'), 2)
+
+    def test_update_existing_key(self):
+        ht = HashTable(10)
+        ht.put('a', 1)
+        self.assertEqual(ht.get('a'), 1)
+
+        ht.put('a', 3)
+        self.assertEqual(ht.get('a'), 3)
+
+    def test_key_not_found(self):
+        ht = HashTable(10)
+        with self.assertRaises(KeyError):
+            ht.get('nonexistent_key')
+
+    def test_large_dataset(self):
+        ht = HashTable(1000)
+        for i in range(1000):
+            ht.put(str(i), i)
+
+        for i in range(1000):
+            self.assertEqual(ht.get(str(i)), i)
 
 if __name__ == '__main__':
-	unittest.main()
-	
+    unittest.main()
